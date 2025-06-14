cbf2fc4fe095bce2a2c2c9ae608ce77194052367
Matt Joyce <matt.joyce@gmail.com>
2025-06-14 22:48:35 +0000

    changed port

--- google_oauth.py
+++ google_oauth.py
@@ -540,4 +540,4 @@ async def get_token_status():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=9000)
+    uvicorn.run(app, host="127.0.0.1", port=9001)
